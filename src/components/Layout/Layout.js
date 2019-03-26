3514d94820f1b930a3b46b034d2c772d42f14ce0
Juan Allo Ron <2070972+juanallo@users.noreply.github.com>
2019-03-25 18:13:28 -0700

    adding ingredients

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,9 @@
 import React, {Fragment} from 'react';
+import classes from './Layout.module.css';
 const Layout = (props) => (
 	<Fragment>
 		<div>Toolbar, SideDrawer, BackDrop</div>
-		<main>
+		<main className={classes.content}>
 			{props.children}
 		</main>
 	</Fragment>
