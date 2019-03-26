26cab17e261d72ca8851f0ef3482c4a08bebcc0a
Juan Allo Ron <2070972+juanallo@users.noreply.github.com>
2019-03-25 18:55:58 -0700

    adding controls based on types

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -2,11 +2,20 @@ import React from 'react';
 import classes from './BuildControls.module.css'
 import BuildControl from "./BuildControl/BuildControl";
 
+const controls = [
+	{label: 'Meat', type: 'meat'},
+	{label: 'Salad', type: 'salad'},
+	{label: 'Cheese', type: 'cheese'},
+	{label: 'Bacon', type: 'bacon'}
+];
+
 const BuildControls = (props) => {
 
 	return (
 		<div className={classes.BuildControls}>
-			<BuildControl label={'test'}/>
+			{controls.map((c) => {
+				return <BuildControl key={c.label} label={c.label}/>
+			})}
 		</div>
 	);
 };
