6e33acd296966fcfc52e2c3b1642c37ba1d33263
Juan Allo Ron <2070972+juanallo@users.noreply.github.com>
2019-03-26 16:52:50 -0700

    adding navigation

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Toolbar.module.css'
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const Toolbar = (props) => {
 
@@ -9,7 +10,7 @@ const Toolbar = (props) => {
 			<div>MENU</div>
 			<Logo />
 			<nav>
-				<ul></ul>
+				<NavigationItems />
 			</nav>
 		</header>
 	);
