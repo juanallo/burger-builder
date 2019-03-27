3a545d7f7e6c9ab30afce9465a6e279194d5b673
Juan Allo Ron <2070972+juanallo@users.noreply.github.com>
2019-03-26 22:44:28 -0700

    improving the performance by avoiding the rendering of the modal until is shown

--- src/components/common/Modal/Modal.js
+++ src/components/common/Modal/Modal.js
@@ -1,19 +1,28 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 import classes from './Modal.module.css';
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = (props) => (
-	<Fragment>
-		<Backdrop show={props.show} onClick={props.onModelClosed}/>
-		<div className={classes.Modal}
-		     style={{
-			     transform: props.show ? 'translate(-50%, -50%)': 'translate(-50%, -200vh)',
-			     opacity: props.show ? '1': '0'
-		     }}
-		>
-			{props.children}
-		</div>
-	</Fragment>
-);
 
+class Modal extends Component {
+
+	shouldComponentUpdate(nextProps){
+		return nextProps.show !== this.props.show;
+	}
+
+	render(){
+		return (
+			<Fragment>
+				<Backdrop show={this.props.show} onClick={this.props.onModelClosed}/>
+				<div className={classes.Modal}
+				     style={{
+					     transform: this.props.show ? 'translate(-50%, -50%)' : 'translate(-50%, -200vh)',
+				     opacity: this.props.show ? '1': '0'
+			     }}
+				>
+					{this.props.children}
+				</div>
+			</Fragment>
+		)
+	}
+}
 export default Modal;
